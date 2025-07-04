21f693f78513803c84d531941c3f66acd860f747
AppTesterReview <PrinTikPlatform@yandex.ru>
2025-07-04 15:20:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from fastapp import App
+
+def get_incomes(sum_str: str) -> int | None:
+  """Приводим сумму строкой к числу"""
+  sum_str.replace(" ", "")
+
+  if sum_str.isdigit():
+    return int(sum_str)
